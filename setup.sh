8e90f0453bcbb7331a601b8a07a46dd84bc33bed
Brenno A <brennoaltair13@gmail.com>
2026-02-01 01:17:12 -0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -22,6 +22,9 @@ else
     fi
 fi
 
+mkdir -p ~/.config/aerospace
+cp ./aerospace.toml ~/.config/aerospace/aerospace.toml
+
 defaults write com.apple.dock autohide-delay -float 0; 
 defaults write com.apple.dock autohide-time-modifier -float 0; 
 killall Dock
