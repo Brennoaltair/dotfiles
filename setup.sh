5564cd22c25431a57a9d3d5d4c7ec2df009b30bc
Brenno A <brennoaltair13@gmail.com>
2026-02-01 00:44:51 -0300

    disable aerospace config

--- setup.sh
+++ setup.sh
@@ -22,10 +22,6 @@ else
     fi
 fi
 
-mkdir -p ~/.config/aerospace
-cp ./aerospace.toml ~/.config/aerospace/aerospace.toml
-
-
 defaults write com.apple.dock autohide-delay -float 0; 
 defaults write com.apple.dock autohide-time-modifier -float 0; 
 killall Dock
